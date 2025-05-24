a7423a390653359198dadd9e3d18775098baf6e5
LewisRRamsey <lewis.ramsey18@gmail.com>
2025-05-24 14:35:07 +0100

    add cifar-10-python.tar

---
+++ test.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from train import NeuralNetwork
+
+PATH = './pytorch_trained_model.pth'
+my_neural_network = NeuralNetwork()
+my_neural_network.load_state_dict(torch.load(PATH, weights_only=True))
+
+test_transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 20
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False)
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = my_neural_network(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
